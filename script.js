7cb64c0bc47970b09671abe659b28323e103410c
ShubhamTomer773 <shubhamtomer1773@gmail.com>
2024-09-22 13:05:39 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
 
-    // Log initial elements
+    
     console.log('Screen element:', screen);
     console.log('Buttons:', buttons);
     console.log('Clear button:', clear);
@@ -13,67 +13,33 @@
     buttons.forEach(function(button) {
         button.addEventListener('click', function(e) {
             let value = e.target.dataset.num;
-            console.log('Button clicked:', value); // Debug log
-            if (value !== undefined) { // Ensure value is not undefined
+            console.log('Button clicked:', value); 
+            if (value !== undefined) { 
                 screen.value += value;
-                console.log('Screen value:', screen.value); // Debug log
+                console.log('Screen value:', screen.value); 
             }
         });
     });
 
     equal.addEventListener('click', function(e) {
-        console.log('Equal button clicked'); // Debug log
+        console.log('Equal button clicked'); 
         if (screen.value === '') {
             screen.value = "Please enter";
         } else {
             try {
                 let answer = eval(screen.value);
                 screen.value = answer;
-                console.log('Calculation result:', answer); // Debug log
+                console.log('Calculation result:', answer); 
             } catch (error) {
                 screen.value = "Error";
-                console.log('Calculation error:', error); // Debug log
+                console.log('Calculation error:', error);
             }
         }
     });
 
     clear.addEventListener('click', function(e) {
-        console.log('Clear button clicked'); // Debug log
+        console.log('Clear button clicked'); 
         screen.value = "";
     });
 })();
-// const inputValue = document.getElementById("user-input");
-// const number = document.querySelectorAll(".numbers").forEach(function (item) {
-//     item.addEventListener("click", function (e) {
-//       if (inputValue.innerText === "NaN") {
-//         inputValue.innerText = "";
-//       }
-//       if (inputValue.innerText === "0") {
-//         inputValue.innerText = "";
-//       }
-//       inputValue.innerText += e.target.innerHTML.trim();
-//     });
-//   });
-//   const calculate = document
-//   .querySelectorAll(".operations")
-//   .forEach(function (item) {
-//     item.addEventListener("click", function (e) {
-//       console.log(e.target.innerHTML);
-//     });
-//   });
-//   let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
 
-// if (!isNaN(lastValue) && e.target.innerHTML === "=") {
-//     inputValue.innerText = eval(inputValue.innerText);
-// } else if (e.target.innerHTML === "AC") {
-//     inputValue.innerText = 0;
-// } else if (e.target.innerHTML === "DEL") {
-//     inputValue.innerText = inputValue.innerText.substring(0,inputValue.innerText.length - 1);
-//     if (inputValue.innerText.length == 0) {
-//         inputValue.innerText = 0;
-//     }
-// } else {
-//     if (!isNaN(lastValue)) {
-//         inputValue.innerText += e.target.innerHTML;
-//     }
-// }
